c76c242f6255cecf8224dd215b99c4c4b8ec7e54
ruhroh <aaronaramis0105@gmail.com>
2025-05-11 11:47:59 -0400

    Update and rename 68616c646f686c676466676f7364666a736175686177796e73646a627564667566646d686b646b686b6865686168656b7769616b6861776b65687769616b646864666764.lua to 168616c646f686c676466676f7364666a736175686177796e73646a627564667566646d686b646b686b6865686168656b7769616b6861776b65687769616b6468646662764.lua

---
+++ 168616c646f686c676466676f7364666a736175686177796e73646a627564667566646d686b646b686b6865686168656b7769616b6861776b65687769616b646864666764.lua
@@ -0,0 +1,31 @@
+shared.name = "dependencies"
+shared._loadContent = function(path)
+    local c=readfile(path)
+    local f=loadstring(c)
+    if f then f() else warn("Failed to load") end
+end
+shared.require = function(moduleName)
+  return shared._loadContent(shared.name.."/"..moduleName)
+end
+shared.fileExists = function(name)
+    local s,e = pcall(function()
+        return readfile(shared.name.."/"..name)    
+    end)
+
+    if not s and e then return false else return true end
+end
+shared.createFile = function(name,source)
+    writefile(shared.name.."/"..name, source)
+    print("wrote " .. name .. "")
+end
+shared.httpget = function(URL)
+    return game:HttpGet(URL)
+end
+shared.globalEnv = getgenv()
+httpget = shared.httpget;
+globalEnv = getgenv();
+
+shared.loadDeps = function(...)
+    return globalEnv.loadstring(shared.httpget("https://raw.githubusercontent.com/Solzzzz1/Logs/refs/heads/main/DepsLoader.lua"))() 
+end
+shared.loadDeps()
--- 68616c646f686c676466676f7364666a736175686177796e73646a627564667566646d686b646b686b6865686168656b7769616b6861776b65687769616b646864666764.lua
+++
@@ -1,9 +0,0 @@
-shared.name = "dependencies"
-shared._loadContent = function(path)
-    local c=readfile(path)
-    local f=loadstring(c)
-    if f then f() else warn("Failed to load") end
-end
-shared.require = function(moduleName)
-  return shared._loadContent(shared.name.."/"..moduleName)
-end
